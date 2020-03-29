e51902e64836d1c1cb01aac2cef1d3cc4e8228d9
LeChatErrant <guillaume.hector@epitech.eu>
2020-03-29 01:59:12 +0100

    misc: permission set to terraform files output

--- provisioning/outputs.tf
+++ provisioning/outputs.tf
@@ -6,4 +6,6 @@ output "ip" {
 resource "local_file" "ansible_inventory" {
   filename = "../my_cluster/terraform_workers_ips"
   content = join(" ", module.digitalocean.ip)
+  file_permission = "0666"
+  directory_permission = "0777"
 }
