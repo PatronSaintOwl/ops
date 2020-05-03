49cb5d3dbb5a960a5ee4feffa14ef7294bd36c09
LeChatErrant <guillaume.hector@epitech.eu>
2020-05-04 01:17:13 +0200

    feat: database - postgreSQL

---
+++ 04_database/clean.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+config_path="$(dirname "$PWD")/my_cluster/kubeconfig"
+
+if [ ! -f "$config_path" ]
+then
+    echo "Kubeconfig not found. You must setup a kubernetes cluster first"
+    exit 1
+fi
+
+export KUBECONFIG=$config_path
+echo "KUBECONFIG set at $config_path"
+echo
+
+# Core
+kubectl delete -f postgres
+
+echo ""
+echo "PostgreSQL database removed!"
